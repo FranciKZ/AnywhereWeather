f3a4fad27e2355b032353ea64b8a18d5488b991d
Kollin Francis <kollinzcfrancis@gmail.com>
2022-07-04 15:09:26 -0400

    Getting go working with .env

--- Dockerfile
+++ Dockerfile
@@ -12,5 +12,6 @@ ADD ./server /serverApp
 WORKDIR /serverApp
 EXPOSE 8080
 COPY --from=clientBuilder /clientApp/build ./clientBuild
+ENV GIN_MODE=release
 RUN go build -o main .
 CMD ["/serverApp/main"]
--- server/main.go
+++ server/main.go
@@ -7,8 +7,10 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 var db = make(map[string]string)
@@ -103,9 +105,15 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	r := gin.Default()
-	//r.SetTrustedProxies([]string{"localhost"})
+	r.SetTrustedProxies([]string{"localhost"})
 	r.Use(static.Serve("/", static.LocalFile("./clientBuild", true)))
+	openWeatherKey := os.Getenv("openWeatherKey")
+
 	api := r.Group("/api")
 	{
 		api.GET("/ping", func(c *gin.Context) {
@@ -115,13 +123,21 @@ func main() {
 			latitude := context.Query("latitude")
 			longitude := context.Query("longitude")
 
-			resp, err := http.Get(fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=", latitude, longitude))
+			resp, err := http.Get(fmt.Sprintf(
+				"https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s",
+				latitude,
+				longitude,
+				openWeatherKey))
+
 			if err != nil {
 				log.Fatalln(err)
 				context.String(http.StatusInternalServerError, "Something went wrong :P")
 			}
+
 			defer resp.Body.Close()
-			body, err := ioutil.ReadAll(resp.Body) // response body is []byte
+
+			body, err := ioutil.ReadAll(resp.Body)
+
 			var result WeatherResponse
 			if err := json.Unmarshal(body, &result); err != nil {
 				fmt.Println("Can not unmarshal JSON")
